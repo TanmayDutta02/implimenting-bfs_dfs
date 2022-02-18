13367faf41e1ac4b77ce7bda74a8288b7347cc19
TANMAY DUTTA <91477406+TanmayDutta02@users.noreply.github.com>
2022-02-18 22:21:33 +0530

    Add files via upload

---
+++ bfs-dfs.py
@@ -0,0 +1,43 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F', 'G'],
+    'D': [],
+    'E': ['H', 'I'],
+    'F': [],
+    'G': ['J'],
+    'H': [],
+    'I': [],
+    'J': [],
+}
+
+visited_bfs = []
+queue = []
+
+def bfs(visited_bfs, graph, node):
+  visited_bfs.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited_bfs:
+        visited_bfs.append(neighbour)
+        queue.append(neighbour)
+
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print (node, end=" ")
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+print("BFS:" , end =" ")
+bfs(visited_bfs, graph, 'A')
+print('\n')
+print("DFS:" , end =" ")
+dfs(visited, graph, 'A')
